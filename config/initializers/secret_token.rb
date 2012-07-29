aa9953fe1016deff80e059ab0ef8ab36e7d9a69b
Oleksandr Bystrikov <coder@yakviter.com.ua>
2012-07-29 18:46:04 +0300

    daybook spectrum clear rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaybookSpectrum::Application.config.secret_token = '150df2b597da79fdf8d55f14ef9d20360d51151315b644767e7b04f241795243f220c07a6e1c38b96e9bd7dfec60cd480b2cbaf89eb2324579999ca50592d870'
