75da8578e9141fcbd5dac0bf170b5eae1add6e67
Oleksandr Bystrikov <coder@yakviter.com.ua>
2012-08-08 13:34:39 +0300

    dog branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730072435) do
+ActiveRecord::Schema.define(:version => 20120803142813) do
 
   create_table "daybooks", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120730072435) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.integer  "character_id"
   end
 
   create_table "scientists", :force => true do |t|
